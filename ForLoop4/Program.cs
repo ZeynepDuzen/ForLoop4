0bd4056f98b2db34a2a2c418f6a1cf1c6f78fa7f
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-08-04 14:19:36 +0300

    Add project files.

---
+++ ForLoop4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // *** kullanicidan negatif deger girilene kadar girilen pozitif degerlerin adeti ve toplamini 
+            //hesaplayan kod blogunu yaziniz ***
+
+            int toplam = 0;
+            int adet = 0;
+
+            // 1.yol
+            do
+            {
+
+                Console.WriteLine("Lütfen bir sayi giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi < 0)
+                {
+                    break;
+                }
+                toplam = toplam + sayi;
+                adet++;
+
+            } while (true);
+
+            Console.WriteLine("Girilen pozitif degrlerin adeti : " + adet);
+            Console.WriteLine("Girilen poziti degerlerin toplami : " + toplam);
+
+            Console.Read();
+        }
+
+        
+    }
+}
